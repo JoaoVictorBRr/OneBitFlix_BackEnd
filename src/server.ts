d43b832fa7e4f11518328339350d29ad6af4ddee
João Victor de Oliveira <jvictor.oliv2000@gmail.com>
2023-05-16 16:48:03 -0300

    Relacionamentos entre Modelos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from "express"
+import { adminJsRouter, adminjs } from "./adminjs"
 import { sequelize } from "./database"
 
+
+
 const app = express()
+
+app.use(express.static('public'))
+
+app.use(adminjs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
