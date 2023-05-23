270af7b73e6a9515ef2996b0953da3ff2911e65b
João Victor de Oliveira <jvictor.oliv2000@gmail.com>
2023-05-23 17:42:59 -0300

    Login com json web token

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,6 +2,8 @@ import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
 import bcrypt  from 'bcrypt'
 
+type CheckPasswordCallback = (err?: Error | undefined, isSame?: boolean) => void
+
 export interface User {
   id: number
   firstName: string
@@ -17,7 +19,9 @@ export interface UserCreationAttributes
   extends Optional<User, 'id'> {}
 
 export interface UserInstance
-  extends Model<User, UserCreationAttributes>, User {}
+  extends Model<User, UserCreationAttributes>, User {
+    checkPassword: (password: string, calbackfn: CheckPasswordCallback) => void
+  }
 
 export const User = sequelize.define<UserInstance, User>('User', {
   id: {
@@ -61,7 +65,9 @@ export const User = sequelize.define<UserInstance, User>('User', {
         isIn: [['admin', 'user']]
     }
   }
-},{
+},
+
+{
     hooks:{
         beforeSave: async (user) => {
             if(user.isNewRecord || user.changed('password')){
@@ -72,3 +78,13 @@ export const User = sequelize.define<UserInstance, User>('User', {
 }
 
 )
+
+User.prototype.checkPassword = function (password: string, callbackfn:  CheckPasswordCallback) {
+  bcrypt.compare(password, this.password, (err, isSame) => {
+    if (err){
+      callbackfn(err)
+    } else {
+      callbackfn(err, isSame)
+    }
+  })
+}
